d06c3c6fc5363c78f9623b84b571e76c1b709c53
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-13 13:45:46 +0000

    Version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/twitter-widget-with-styling/tags/1.1@803797 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,54 @@
+=== Twitter Widget with Styling ===
+Contributors: mpol
+Tags: twitter, widget, twitter style, twitter styling, stylesheet
+Requires at least: 3.0
+Tested up to: 3.7.1
+Stable tag: 1.1
+License: GPLv2 or later
+
+
+A Twitter widget that is easy to configure and easy to style.
+
+== Description ==
+
+A Twitter widget that is easy to configure and easy to style. It uses the Embedded Timeline widget of Twitter.com.
+You can have it load an extra stylesheet, where you can style the layout of the widget.
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to [Twitter](https://dev.twitter.com/docs/embedded-timelines).
+4. Create your own Embedded Timeline App.
+5. Go to Appearance->Widgets, add the widget to any sidebar.
+6. Add your Twitter Name and your Twitter ID from the code you got from Twitter.
+7. Enjoy!
+
+== Frequently Asked Questions ==
+
+= How do I edit the stylesheet? =
+
+Copy the style_twitter.css from the plugin to your theme directory.
+The widget will find it and add it to the webpage instead of the default in the plugin.
+Do not edit the style_twitter.css in the plugin itself, it will be overwritten when updating.
+
+= Can I add multiple widgets to a page? =
+
+You can, but it will probably not load any styling in the second widget.
+
+== Screenshots ==
+
+1. Screenshot of the widget in the admin section.
+2. Screenshot of the frontend of the widget with transparent styling
+3. Screenshot of the frontend of the widget with grey styling
+4. Screenshot of the frontend of the widget with brown styling
+5. Screenshot of the frontend of the widget with blue styling
+6. Screenshot of the frontend of the widget with blue styling
+
+== Changelog ==
+
+= 1.1 =
+* First fix issue of loading on every page
+
+= 1.0 =
+* First Version
---
+++ twitter-widget-with-styling.php
@@ -0,0 +1,171 @@
+<?php
+/*
+Plugin Name: Twitter Widget with Styling
+Plugin URI: http://timelord.nl/
+Description: A Twitter widget that is easy to configure and easy to style.
+Version: 1.1
+Author: Marcel Pol
+Author URI: http://timelord.nl
+Text Domain: twitter_style
+Domain Path: /lang/
+
+
+Copyright 2013 Marcel Pol (email: marcel@timelord.nl)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+class TL_Twitter extends WP_Widget {
+
+	function __construct() {
+		$widget_ops = array('classname' => 'tl_twitter', 'description' => __('Twitter Widget with Styling','twitter_style') );
+		parent::__construct('tl_twitter', 'Twitter Widget', $widget_ops);
+		$this->alt_option_name = 'tl_twitter';
+
+		add_action( 'save_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
+		add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
+
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+
+		load_plugin_textdomain('twitter_style', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/');
+	}
+
+	function widget($args, $instance) {
+		$cache = wp_cache_get('tl_twitter', 'widget');
+
+		if ( !is_array($cache) )
+			$cache = array();
+
+		if ( ! isset( $args['widget_id'] ) )
+			$args['widget_id'] = $this->id;
+
+		if ( isset( $cache[ $args['widget_id'] ] ) ) {
+			echo $cache[ $args['widget_id'] ];
+			return;
+		}
+
+		ob_start();
+		extract($args);
+
+		$title	= apply_filters('widget_title', empty($instance['title']) ? 'Twitter' : $instance['title'], $instance, $this->id_base);
+		$name	= $instance['name'];
+		$id		= $instance['id'];
+		$height	= $instance['height'];
+		$border	= $instance['border'];
+
+		// find out if the template has a stylesheet, else use the one in the plugin
+		$css = TEMPLATEPATH . '/style_twitter.css';
+		if ( file_exists( $css ) ) {
+			$css = get_bloginfo( 'template_url' ) . '/style_twitter.css';
+		} else {
+			$css = WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)) . '/css/style_twitter.css';
+		}
+		$rand = mt_rand(0, 10);
+
+		echo $before_widget;
+		if ( $title ) echo $before_title . $title . $after_title; ?>
+
+		<a class="twitter-timeline"
+			data-border-color="<?php echo $border; ?>"
+			height="<?php echo $height; ?>"
+			data-theme="light"
+			href="https://twitter.com/<?php echo $name; ?>"
+			data-widget-id="<?php echo $id; ?>">Tweets van @<?php echo $name; ?>
+		</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+		<script>
+			/* Twitter Widget with Styling */
+			var css = '<?php echo $css . "?ver=" . $rand; ?>';
+		</script>
+
+		<?php echo $after_widget; ?>
+
+		<?php
+		$cache[$args['widget_id']] = ob_get_flush();
+		wp_cache_set('tl_twitter', $cache, 'widget');
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title']	= strip_tags($new_instance['title']);
+		$instance['name']	= strip_tags($new_instance['name']);
+		$instance['id']		= strip_tags($new_instance['id']);
+		$instance['height'] = strip_tags($new_instance['height']);
+		$instance['border'] = strip_tags($new_instance['border']);
+		$this->flush_widget_cache();
+
+		$alloptions = wp_cache_get( 'alloptions', 'options' );
+		if ( isset($alloptions['tl_twitter']) )
+			delete_option('tl_twitter');
+
+		return $instance;
+	}
+
+	function flush_widget_cache() {
+		wp_cache_delete('tl_twitter', 'widget');
+	}
+
+	function form( $instance ) {
+    	/*
+    	 * Set Default Value for widget form
+    	 */
+    	$default_value	=	array("title"=> "Twitter", "name" => "MarcelPolleke", "id" => "399217180551544832", "height" => 400, "border" => "#f4f4f4" );
+    	$instance		=	wp_parse_args( (array) $instance, $default_value );
+
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : ''; ?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'twitter_style'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p><?php
+
+		$name = isset($instance['name']) ? esc_attr($instance['name']) : ''; ?>
+		<p><label for="<?php echo $this->get_field_id('name'); ?>"><?php _e('Twitter Name', 'twitter_style'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('name'); ?>" type="text" value="<?php echo $name; ?>" /></p><?php
+
+		$id = isset($instance['id']) ? esc_attr($instance['id']) : ''; ?>
+		<p><label for="<?php echo $this->get_field_id('id'); ?>"><?php _e('Twitter ID', 'twitter_style'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('id'); ?>" name="<?php echo $this->get_field_name('id'); ?>" type="text" value="<?php echo $id; ?>" /></p><?php
+
+		$height = isset($instance['height']) ? esc_attr($instance['height']) : ''; ?>
+		<p><label for="<?php echo $this->get_field_id('height'); ?>"><?php _e('Height', 'twitter_style'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo $height; ?>" /></p><?php
+
+		$border = isset($instance['border']) ? esc_attr($instance['border']) : ''; ?>
+		<p><label for="<?php echo $this->get_field_id('border'); ?>"><?php _e('Border Color', 'twitter_style'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('border'); ?>" name="<?php echo $this->get_field_name('border'); ?>" type="text" value="<?php echo $border; ?>" /></p><?php
+	}
+
+	/**
+	 * Register and enqueue scripts.
+	 */
+	public function register_plugin_styles() {
+		// Registers Style with WordPress to wp_footer() so the browser-cache is updated
+		wp_register_script( 'tl_twitter', WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)) . '/js/style_twitter.js','jquery','', true );
+
+		// Adding the js only if widget in use
+		if ( is_active_widget( false, false, $this->id_base, true ) ) {
+			wp_enqueue_script('jquery');
+			wp_enqueue_script('tl_twitter');
+		}
+	}
+}
+
+
+function tl_twitter() {
+	register_widget('TL_Twitter');
+}
+add_action('widgets_init', 'tl_twitter' );
