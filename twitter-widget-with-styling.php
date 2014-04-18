155774bc2b80a94e588e107d32f28def0bcbe5e7
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-18 17:09:07 +0000

    new web and mail address
    git-svn-id: https://plugins.svn.wordpress.org/twitter-widget-with-styling/trunk@897562 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitter-widget-with-styling.php
+++ twitter-widget-with-styling.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Twitter Widget with Styling
-Plugin URI: http://timelord.nl/
+Plugin URI: http://products.zenoweb.nl/free-wordpress-plugins/twitter-widget-styling/
 Description: A Twitter widget that is easy to configure and easy to style.
 Version: 1.2.6
 Author: Marcel Pol
-Author URI: http://timelord.nl
+Author URI: http://zenoweb.nl
 Text Domain: twitter_style
 Domain Path: /lang/
 
@@ -104,7 +104,7 @@ class TL_Twitter extends WP_Widget {
 		// Registers Style with WordPress to wp_footer() so the browser-cache is updated
 		wp_register_script( 'tl_twitter', WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)) . '/js/style_twitter.js','jquery','1.2.5', true );
 		wp_enqueue_script('tl_twitter');
-		
+
 		$cache[$args['widget_id']] = ob_get_flush();
 		wp_cache_set('tl_twitter', $cache, 'widget');
 	}
