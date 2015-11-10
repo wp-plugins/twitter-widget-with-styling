7b21cbadea595e6b7a7be31da0bc3db41cf626b5
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-10 11:58:12 +0000

    Tag 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/twitter-widget-with-styling/tags/2.0.0@1283237 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/style_twitter.js
+++ js/style_twitter.js
@@ -1,64 +1,123 @@
-	function styleTwitterWidget() {
-		var checkTwitter = 0;
-		var ifrm_elem = 0;
-		var ifrm_content = 0;
-		var ifrm = 0;
-		if(typeof twitter_style_css == "undefined") {
-			return;
-		} else {
-			var cssUrl = twitter_style_css;
-		}
 
-		var cssLink = jQuery("<link/>", {
-			href: cssUrl,
-			rel: "stylesheet",
-			type: "text/css",
-			name: "twitterCSS"
-		});
+/*
+ * Twitter Widget with Styling
+ * Version: 2.0.0
+ *
+ * Github URL: https://github.com/MPolleke/styleTwitterWidget
+ *
+ * Author: Marcel Pol
+ * Author URL: https://zenoweb.nl
+ * Author Email: marcel@timelord.nl
+ * Copyright: 2013 - 2015, Marcel Pol
+ * License: MIT
+ *
+ * "To iterate is human, to recurse divine."
+ * (L Peter Deutsch)
+ *
+ */
 
-		if (jQuery('.twitter-timeline').length > 0) {
-			checkTwitter++;
-			if ( !document.getElementById('twitter-widget-0') ) {
-				// add an id if it doesn't exists already
-				jQuery('.twitter-timeline').prop('id', 'twitter-widget-0');
-			}
-			// add a name to the iframe
-			jQuery('#twitter-widget-0').attr('name', 'twitter-widget-0');
 
-			//if (typeof head == "object") {
-				ifrm_elem = document.getElementById('twitter-widget-0');
-				if (typeof ifrm_elem.contentWindow == 'object') {
-					ifrm_content = ifrm_elem.contentWindow;
-					if (typeof ifrm_content.document == 'object') {
-						ifrm = ifrm_content.document;
+/* Standard code taken from Twitter website. */
+!function(d,s,id) {
+	var js,fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http':'https';
+	if ( !d.getElementById(id) ) {
+		js = d.createElement(s);
+		js.id = id;
+		js.src = p+"://platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore( js, fjs );
+	}
+} ( document, "script", "twitter-wjs" );
 
-						var head = ifrm.head;
-						var jquery_head = jQuery("#twitter-widget-0").contents().find("head");
-						var ss = ifrm.styleSheets;
-						if (typeof ss == "object") {
-							for (var i = 0, max = ss.length; i < max; i++) {
-								if (ss[i].href == cssUrl) {
-									// it was already added, do nothing from now on...
-									checkTwitter = 11;
-									return;
-								}
-							}
+
+/*
+ * Recursive function to add a stylesheet to the iframe.
+ *
+ * Parameters:
+ * - widget: the number of the available widget, starts at 0, autoincrement.
+ * - counter: the number of times this function was called for this widget. Starts at 0. Will return after maximum 15 times.
+ *
+ */
+function styleTwitterWidget_v2( widget, counter ) {
+
+	counter++;
+
+	// Location of the style_twitter.css, public url. Change this according to your needs.
+	// var cssUrl = 'http://example.com/style_twitter_css';
+	var cssUrl = tl_twitter_localize.style_twitter_css; // WordPress variable, gets localized.
+
+	var cssLink = jQuery("<link/>", {
+		href: cssUrl,
+		rel: "stylesheet",
+		type: "text/css",
+		name: "twitterCSS"
+	});
+
+	if ( jQuery( '#twitter-widget-' + widget ).length > 0 ) {
+		var ifrm_elem = jQuery( '#twitter-widget-' + widget );
+		var ifrm_content = document.getElementById( 'twitter-widget-' + widget ).contentWindow;
+
+		if ( typeof ifrm_content == 'object' ) {
+			// console.log( widget + ' We have content...' );
+			if ( typeof ifrm_content.document == 'object' ) {
+				var ifrm_doc = ifrm_content.document;
+
+				var jquery_head = jQuery( '#twitter-widget-' + widget ).contents().find( 'head' );
+				var stylesheet = ifrm_doc.styleSheets;
+				// console.log( widget + ' typeof: ' + typeof stylesheet );
+
+				if ( typeof stylesheet == 'object' ) {
+					for ( var i = 0, max = stylesheet.length; i < max; i++ ) {
+						if ( stylesheet[i].href == cssUrl ) {
+							// console.log( widget + ' We have the stylesheet: ' + stylesheet[i].href );
+
+							// It was already added, do nothing from now on...
+							counter = 16;
+							// console.log( widget + ' Returning on a counter of: ' + counter );
+							return;
 						}
-						// add the stylesheet
-						jquery_head.append(cssLink);
 					}
 				}
-			//}
 
-			// Ensures it's checked at least 10 times
-			if (checkTwitter < 10) {
-				setTimeout('styleTwitterWidget()', 200);
+				// Add the stylesheet to this iframe
+				jquery_head.append( cssLink );
 			}
-		} else {
-			setTimeout('styleTwitterWidget()', 200);
+		}
+
+		// Ensures it's checked at least 15 times
+		if ( counter < 15 ) {
+			// console.log( widget + ' Iframe has not the right content yet: ' + counter );
+			setTimeout(
+				function() {
+					styleTwitterWidget_v2( widget, counter )
+				}, 200 );
+		}
+	} else {
+		if ( counter < 15 ) {
+			// console.log( widget + ' No Iframe yet: ' + counter );
+			setTimeout(
+				function() {
+					styleTwitterWidget_v2( widget, counter )
+				}, 200 );
 		}
 	}
+}
 
-	jQuery(document).ready(function() {
-		styleTwitterWidget();
+
+/*
+ * Initial call for each Widget.
+ * In the HTML there will be a a.twitter-timeline, that gets replaced by an iframe.twitter-timeline.
+ */
+jQuery(document).ready(function() {
+
+	var widget = 0;
+
+	// Needs to use '.twitter-timeline', not '.twitter-timeline-rendered' to handle slow rendering browsers (hits the "second recurse").
+	jQuery( '.twitter-timeline' ).each( function( element ) {
+
+		//console.log( 'Widget ' + widget + ': Init' );
+
+		styleTwitterWidget_v2( widget, 0 );
+		widget++;
+
 	});
+});
